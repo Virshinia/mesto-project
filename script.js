e565f1545c75b693cc5fd3f70247fcc9c8c5a05e
Nastya Razzhivina <virshinia@yandex.ru>
2022-04-20 08:09:18 +0300

    разработка формы редактирования

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let buttonEdit = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let buttonClose = document.querySelector('.popup__close-button');
+let submitButton = popup.querySelector('.popup__submit-button');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
 function openEditForm () {
     popup.classList.add ('popup_opened');
 }
+
 function closeEditForm () {
     popup.classList.remove ('popup_opened');
 }
 buttonEdit.addEventListener('click', openEditForm);
 buttonClose.addEventListener('click', closeEditForm);
 
+let inputName = popup.querySelector ('#name');
+let inputDescription = popup.querySelector ('#description');
+
+function editInput (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    closeEditForm ();
+}
+
+submitButton.addEventListener('click', editInput)
