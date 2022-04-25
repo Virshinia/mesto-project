42f669ce90b0a2c7d376a6de0eb6c948250a7694
Nastya Razzhivina <virshinia@yandex.ru>
2022-04-26 01:27:27 +0300

    разработка формы по добавлению места

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let profile = document.querySelector('.profile'),
-    buttonsOfProfile = profile.querySelectorAll('button'),
+    buttonEdit = profile.querySelector('.profile__button-edit'),
+    buttonAdd = profile.querySelector('.profile__button-add'),
     buttonsClose = document.querySelectorAll('.popup__close-button'),
-    buttonsLike = document.querySelectorAll('.location__like-icon'),
-    popup = document.querySelectorAll('.popup'),
-    popupContainer = document.querySelectorAll('.popup__container'),
+    popupEdit = document.querySelector('.popup_edit'),
+    popupAdd = document.querySelector('.popup_addPlace'),
+    popupContainerEditForm = document.querySelector('[name="edit-profile"]'),
+    popupContainerAddForm = document.querySelector('[name="add-place"]'),
     profileName = document.querySelector('.profile__name'),
     profileDescription = document.querySelector('.profile__description'),
     inputName = document.querySelector('#name'),
     inputDescription = document.querySelector('#description')
 
-function openForm (index) {
-    popup[index].classList.add ('popup_opened');
-    if (index === 0) {
+const locations = document.querySelector('.locations');
+
+function openEditForm () {
+    popupEdit.classList.add ('popup_opened');
     inputName.setAttribute('placeholder', profileName.textContent);
     inputDescription.setAttribute('placeholder', profileDescription.textContent);
-    }
+}
+
+function openAddForm () {
+    popupAdd.classList.add ('popup_opened');
 }
 
 function closeForm (evt) {
@@ -27,18 +33,39 @@ function closeForm (evt) {
     }
 }
 
-function submitForm (evt) {
+function submitEditForm (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
-    closeForm ();
+    closeForm (evt);
+}
+
+function submitAddForm (evt) {
+    evt.preventDefault();
+    let inputNameOfPlace = popupAdd.querySelector('#nameOfPlace');
+    let inputLinkImg = popupAdd.querySelector('#linkImg');
+
+    addLocation (inputNameOfPlace, inputLinkImg);
+    closeForm (evt);
 }
 
 function toggleLike(evt) {
     evt.target.classList.toggle('location__like-icon_active');
 }
 
-buttonsLike.forEach((button) => button.addEventListener('click', (evt) => toggleLike (evt)));
+function addLocation (inputNameOfPlace, inputLinkImg) {
+    const locationTemplate = document.querySelector('#location-template').content;
+    const locationElement = locationTemplate.querySelector('.location').cloneNode(true);
+    locationElement.querySelector('.location__like-icon').addEventListener('click', evt => toggleLike(evt));
+    locationElement.querySelector('.location__photo').src = inputLinkImg.value;
+    locationElement.querySelector('.location__photo').setAttribute('alt', inputNameOfPlace.value);
+    locationElement.querySelector('.location__title').textContent = inputNameOfPlace.value;
+    locations.append(locationElement);
+}
+
+
 buttonsClose.forEach ((button) => button.addEventListener('click', (evt) => closeForm(evt)));
-buttonsOfProfile.forEach ((button, index) => button.addEventListener('click', function() {openForm(index)}));
-popupContainer.forEach ((button) => button.addEventListener('submit', submitForm));
+buttonAdd.addEventListener('click', openAddForm);
+buttonEdit.addEventListener('click', openEditForm);
+popupContainerEditForm.addEventListener('submit', submitEditForm);
+popupContainerAddForm.addEventListener('submit', submitAddForm);
