d12e40bd3e37dd7e2d662224ef3a016c1c185981
Nastya Razzhivina <virshinia@yandex.ru>
2022-04-25 23:25:14 +0300

    разработка форм, коректировка функции лайков

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-let buttonEdit = document.querySelector('.profile__button-edit');
-let popup = document.querySelector('.popup');
-let buttonClose = document.querySelector('.popup__close-button');
-let editForm = popup.querySelector('.popup__container');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let inputName = popup.querySelector ('#name');
-let inputDescription = popup.querySelector ('#description');
+let profile = document.querySelector('.profile'),
+    buttonsOfProfile = profile.querySelectorAll('button'),
+    buttonsClose = document.querySelectorAll('.popup__close-button'),
+    buttonsLike = document.querySelectorAll('.location__like-icon'),
+    popup = document.querySelectorAll('.popup'),
+    popupContainer = document.querySelectorAll('.popup__container'),
+    profileName = document.querySelector('.profile__name'),
+    profileDescription = document.querySelector('.profile__description'),
+    inputName = document.querySelector('#name'),
+    inputDescription = document.querySelector('#description')
 
-function openEditForm () {
-    popup.classList.add ('popup_opened');
+function openForm (index) {
+    popup[index].classList.add ('popup_opened');
+    if (index === 0) {
     inputName.setAttribute('placeholder', profileName.textContent);
     inputDescription.setAttribute('placeholder', profileDescription.textContent);
+    }
 }
 
-function closeEditForm () {
-    popup.classList.remove ('popup_opened');
-    inputName.value = '';
-    inputDescription.value = '';
+function closeForm (evt) {
+    let formContainer =  evt.target.parentElement.parentElement;
+    formContainer.classList.remove ('popup_opened');
+
+    if (formContainer.classList.contains('popup_edit')) {
+        inputName.value = '';
+        inputDescription.value = '';
+    }
 }
 
 function submitForm (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
-    closeEditForm ();
+    closeForm ();
 }
 
-buttonEdit.addEventListener('click', openEditForm);
-buttonClose.addEventListener('click', closeEditForm);
-editForm.addEventListener('submit', submitForm);
+function toggleLike(evt) {
+    evt.target.classList.toggle('location__like-icon_active');
+}
+
+buttonsLike.forEach((button) => button.addEventListener('click', (evt) => toggleLike (evt)));
+buttonsClose.forEach ((button) => button.addEventListener('click', (evt) => closeForm(evt)));
+buttonsOfProfile.forEach ((button, index) => button.addEventListener('click', function() {openForm(index)}));
+popupContainer.forEach ((button) => button.addEventListener('submit', submitForm));
